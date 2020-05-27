d5a7d4b7c5b01b37f9fad507e78f69b05fa737e0
Homin Lee <homin.lee@suapapa.net>
2020-05-28 05:54:48 +0900

    morp implemented

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+)
+
+var (
+	exampleStr = "사랑은 모든것을 덮어주고 모든것을 믿으며 모든것을 바라고 모든것을  견디어냅니다"
 )
 
 func main() {
@@ -10,11 +15,19 @@ func main() {
 	k, err := New("", "")
 	chk(err)
 	defer k.Close()
-	ch := k.Analyze("세상아 안녕", "")
-	for v := range ch {
+
+	var inputStr string
+	if len(os.Args) == 0 {
+		inputStr = exampleStr
+	} else {
+		inputStr = os.Args[1]
+	}
+
+	for v := range k.Analyze(inputStr, "") {
 		log.Println(v.Val())
 		for m := range v.Morphs() {
-			log.Println(m.Lex(), m.Tag(), m.cptr.begin, m.cptr.length)
+			// log.Println(m.Lex(), m.Tag(), m.cptr.begin, m.cptr.length)
+			log.Println("   ", m.Lex(), m.Tag())
 		}
 	}
 }
