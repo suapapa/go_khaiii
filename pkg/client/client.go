3e6f2f9f95e047d85449e1682c76323d96f15010
Homin Lee <ff4500@gmail.com>
2025-05-01 16:06:42 +0900

    add khaiii-api client package

---
+++ pkg/client/client.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/suapapa/khaiii-api/pkg/khaiiitype"
+)
+
+type Client struct {
+	BaseURL string
+	ApiKey  string
+	Client  *http.Client
+}
+
+func (c *Client) Analyze(input string) ([]*khaiiitype.WordChunk, error) {
+	data, err := json.Marshal(map[string]string{
+		"Text": input,
+	})
+	if err != nil {
+		return nil, err
+	}
+	dataReader := bytes.NewReader(data)
+
+	req, err := http.NewRequest("POST", c.BaseURL, dataReader)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if c.ApiKey != "" {
+		req.Header.Set("Authorization", "Bearer "+c.ApiKey)
+	}
+
+	var client *http.Client
+	if c.Client != nil {
+		client = c.Client
+	} else {
+		client = http.DefaultClient
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status code: %d", resp.StatusCode)
+	}
+
+	type RespData struct {
+		Data khaiiitype.AnalyzeResult `json:"data"`
+	}
+	var respData RespData
+	err = json.NewDecoder(resp.Body).Decode(&respData)
+	if err != nil {
+		return nil, err
+	}
+
+	return respData.Data.WordChunks, nil
+}
